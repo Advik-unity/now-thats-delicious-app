eba7cf923cfbf19b1504c4b897a5dc62773f851a
ketan <code.ketan@gmail.com>
2017-05-27 14:48:59 +0530

    Commit #8

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,15 +2,25 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  res.render('index', {title: 'Home'})
+  res.render('index', {title: 'Home'});
 };
 
 exports.addStore = (req, res) => {
-	res.render('editStore', {title: 'Add Store'})
+  res.render('editStore', {title: 'Add Store'});
 };
 
 exports.createStore = async (req, res) => {
-	const store = new Store(req.body);
-	await store.save();
-	res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `${store.name} has been successfully created. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+	const stores = await Store.find();
+	res.render('stores', {title: 'Stores', stores});
+}
+
+exports.editStore = async (req, res) => {
+	const store = await Store.findOne({_id: req.params.id});
+	res.render('editStore', {title: `Edit ${store.name}`, store});
+}
--- models/Store.js
+++ models/Store.js
@@ -3,25 +3,26 @@ mongoose.Promise = global.Promise;
 const slug = require('slugs');
 
 const storeSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		trim: true,
-		required: 'Please enter a store name.'
-	},
-	slug: String,
-	description: {
-		type: String,
-		trim: true
-	},
-	tags: [String]
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
 });
 
 storeSchema.pre('save', function(next) {
-	if (!this.isModified('name')) {
-		return next();
-	}
-	this.slug = slug(this.name);
-	next();
+  if (!this.isModified('name')) {
+    next();
+    return;
+  }
+  this.slug = slug(this.name);
+  next();
 });
 
 module.exports = mongoose.model('Store', storeSchema);
